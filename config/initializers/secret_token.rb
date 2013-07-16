4dbcec2d0dda71129546192f35985d2db074b180
Sandesh Sethi <nictesh@gmail.com>
2013-07-16 13:56:18 +0530

    Initial commit with modified gemfile and rails app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytwitter::Application.config.secret_token = '07c883dc21413708f5d1a9fdc5f9099ff3a3b935668caa51d700211cf18a92c96eefaad0dc45c2675956c8d3942bde4875a013cb5f07cd9244932e38d05dbd8e'
